96e526cce7080a343cde5e4226b2a7ac49401ef7
Jujhar Singh <jujhar@jujhar.com>
2019-10-18 15:06:28 +0100

    committing

---
+++ .history/server/index_20191017201724.js
@@ -0,0 +1,20 @@
+const http = require('http');
+const port = process.env.NODE_PORT || 3000;
+const version = process.env.VERSION || 'unknown';
+
+const requestHandler = (request, response) => {
+  response.setHeader('Content-Type', 'application/json');
+  // see https://httpstatuses.com/
+  response.statusCode = 200;
+  const out = { message: 'Your friendly JSON server', 'version': version };
+  response.end(JSON.stringify(out));
+};
+
+const server = http.createServer(requestHandler);
+
+server.listen(port, (err) => {
+  if (err) {
+    console.error('oops', err);
+  }
+  console.log(`server v.${version} is listening on ${port}`);
+});
---
+++ .history/server/index_20191018101806.js
@@ -0,0 +1,20 @@
+const http = require('http');
+const port = process.env.NODE_PORT || 3000;
+const version = process.env.VERSION || 'unknown';
+
+const requestHandler = (request, response) => {
+  response.setHeader('Content-Type', 'application/json');
+  response.statusCode = 200;
+  const out = { message: 'Your friendly JSON server', 'version': version };
+  console.debug("I hit this off");
+  response.end(JSON.stringify(out));
+};
+
+const server = http.createServer(requestHandler);
+
+server.listen(port, (err) => {
+  if (err) {
+    console.error('oops', JSON.stringify(err));
+  }
+  console.log(`server v.${version} is listening on ${port}`);
+});
